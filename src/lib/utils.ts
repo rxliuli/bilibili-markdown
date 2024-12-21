7f4402e1f27e8679c05bcdb0d0fee90d2b278baf
rxliuli <rxliuli@gmail.com>
2024-12-22 00:59:46 +0800

    fix: update thematicBreak

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,28 +1,12 @@
-import { fromHtml } from 'hast-util-from-html'
-import { toMdast } from 'hast-util-to-mdast'
 import { fromMarkdown } from 'mdast-util-from-markdown'
-import { toMarkdown } from 'mdast-util-to-markdown'
-import { toHtml } from 'hast-util-to-html'
 import { select } from 'unist-util-select'
 import type { Heading, Root } from 'mdast'
-import { gfmFromMarkdown, gfmToMarkdown } from 'mdast-util-gfm'
+import { gfmFromMarkdown } from 'mdast-util-gfm'
 import { gfm } from 'micromark-extension-gfm'
-import { toHast } from 'mdast-util-to-hast'
 import type Quill from 'quill'
 import { toString } from 'mdast-util-to-string'
 import markdownToQuillDelta from 'markdown-to-quill-delta'
-
-export function html2md(html: string): string {
-  const hast = fromHtml(html, { fragment: true })
-  const mdast = toMdast(hast)
-  return toMarkdown(mdast)
-}
-
-export function md2html(md: string): string {
-  const hast = fromMarkdown(md)
-  const html = toHtml(hast as any)
-  return html
-}
+import type { Handle } from 'markdown-to-quill-delta'
 
 function updateTitle(title: string) {
   // 获取 textarea 元素
@@ -43,35 +27,32 @@ function updateTitle(title: string) {
   textarea.dispatchEvent(event)
 }
 
+const thematicBreak: Handle = ({ node, ops }) => {
+  if (node.type !== 'thematicBreak') {
+    return
+  }
+  ops.push({
+    attributes: {
+      class: 'cut-off',
+    },
+    insert: {
+      'cut-off': {
+        type: '0',
+        url: 'https://i0.hdslb.com/bfs/article/0117cbba35e51b0bce5f8c2f6a838e8a087e8ee7.png',
+      },
+    },
+  })
+  return true
+}
+
 function updateContent(root: Root) {
-  // console.log('updateContent', root)
   const quill = (document.querySelector('.rql-editor') as any).__quill as Quill
   if (!quill) {
     throw new Error('未找到 quill 实例')
   }
   quill.setContents(
     markdownToQuillDelta(root, {
-      handle: ({ node, ops }) => {
-        if (node.type === 'thematicBreak') {
-          ops.push(
-            {
-              attributes: {
-                class: 'cut-off',
-              },
-              insert: {
-                'cut-off': {
-                  type: '0',
-                  url: 'https://i0.hdslb.com/bfs/article/0117cbba35e51b0bce5f8c2f6a838e8a087e8ee7.png',
-                },
-              },
-            },
-            {
-              insert: '\n',
-            },
-          )
-          return true
-        }
-      },
+      handle: thematicBreak,
     }),
   )
 }
